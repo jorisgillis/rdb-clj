12c7179154a4817aeb848858743de48c6407c57a
Joris Gillis <joris.gillis@dsquare.be>
2016-07-22 14:21:17 +0200

    [ISSUE-1] Initial structure of Elm App

---
+++ frontend/App.elm
@@ -0,0 +1,45 @@
+module App exposing (..)
+
+import Basics exposing (multiply, subtract, divide)
+import Html exposing (Html, div, text)
+import Html.App
+
+-- Model
+type alias Model =
+    String
+
+init : (Model, Cmd Msg)
+init = ("Hello", Cmd.none)
+
+-- MESSAGES
+type Msg
+    = NoOp
+
+-- VIEW
+view : Model -> Html Msg
+view model =
+    div []
+        [text model]
+
+-- UPDATE
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        NoOp ->
+            (model, Cmd.none)
+
+-- SUBSCRIPTIONS
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+-- MAIN
+main : Program Never
+main =
+    Html.App.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+        
